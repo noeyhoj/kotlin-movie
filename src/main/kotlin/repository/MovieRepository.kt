ff49c855adac47a0f8dd4b99dda9d251b3709278
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-04-17 00:02:58 +0900

    feat: DB에서 영화 조회

---
+++ src/main/kotlin/repository/MovieRepository.kt
@@ -0,0 +1,25 @@
+package repository
+
+import db.DatabaseConfig
+import model.movie.Movie
+
+class MovieRepository {
+    fun findByTitle(title: String): Movie {
+        val sql = "SELECT * FROM MOVIE WHERE title = ?"
+        DatabaseConfig.getConnection().use { connection ->
+            connection.prepareStatement(sql).use { stmt ->
+                stmt.setString(1, title)
+                val rs = stmt.executeQuery()
+                if (rs.next()) {
+                    return Movie(
+                        title = rs.getString("title"),
+                        runningTime = rs.getInt("running_time").toLong(),
+                        startDate = rs.getDate("start_date").toLocalDate(),
+                        endDate = rs.getDate("end_date").toLocalDate(),
+                    )
+                }
+            }
+        }
+        throw IllegalArgumentException("존재하지 않는 영화입니다")
+    }
+}
